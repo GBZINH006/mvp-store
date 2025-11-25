dc43d967e2b3f65a571fbfd617bcc506c14d2834
miguelsscsc <mgoncalvesdeassuncao@gmail.com>
2025-11-25 10:43:01 -0300

    Update main.jsx

--- mvp-store/src/main.jsx
+++ mvp-store/src/main.jsx
@@ -1,27 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { CartProvider } from "./context/CartContext.jsx";
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './context/CartContext.jsx';
 
-// PrimeReact styles
-import "primereact/resources/themes/lara-light-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
 
-// global
-import "./styles/global.css";
+import 'primereact/resources/themes/lara-light-purple/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+
+
+import './styles/global.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+<CartProvider>
+<App />
+</CartProvider>
+</BrowserRouter>
 );
