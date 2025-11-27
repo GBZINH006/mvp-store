3f572e4cb343ea6eb49721aac7f63b52cd0a57c7
GBZINH006 <gabrieldossantosdutra06@gmail.com>
2025-11-27 13:44:50 -0300

    comit

--- mvp-store/src/main.jsx
+++ mvp-store/src/main.jsx
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-=======
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.jsx';
->>>>>>> ff4c5a48729a7abffd26c8ee099bf7062935bf00
 
 
-<<<<<<< HEAD
+
+
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -18,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-=======
+
 import 'primereact/resources/themes/lara-light-purple/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -36,4 +33,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 </CartProvider>
 </BrowserRouter>
 );
->>>>>>> ff4c5a48729a7abffd26c8ee099bf7062935bf00
+
